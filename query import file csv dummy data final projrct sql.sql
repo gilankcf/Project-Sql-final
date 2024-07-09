e9418e018938ade8f81a71309de8f83bf2f0420c
gilankcf <158147635+gilankcf@users.noreply.github.com>
2024-07-09 07:03:53 +0700

    Add files via upload

---
+++ query import file csv dummy data final projrct sql.sql
@@ -0,0 +1,24 @@
+COPY Cities
+FROM 'C:\Users\Rohman Suhartono\Downloads\dat dummy tugas final\city.csv'
+CSV
+HEADER;
+
+COPY Users
+FROM 'C:\Users\Rohman Suhartono\Downloads\dat dummy tugas final\users.csv'
+CSV
+HEADER;
+
+COPY Cars
+FROM 'C:\Users\Rohman Suhartono\Downloads\dat dummy tugas final\cars.csv'
+CSV
+HEADER;
+
+COPY Ads
+FROM 'C:\Users\Rohman Suhartono\Downloads\dat dummy tugas final\ads.csv'
+CSV
+HEADER;
+
+COPY Bids
+FROM 'C:\Users\Rohman Suhartono\Downloads\dat dummy tugas final\bids.csv'
+CSV
+HEADER;
